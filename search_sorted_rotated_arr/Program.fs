27067dfd47900b1a4c1f6e5aa5c7e24739a6735a
ttay <ttay@umich.edu>
2019-10-04 23:04:18 -0400

    finished find

--- search_sorted_rotated_arr/Program.fs
+++ search_sorted_rotated_arr/Program.fs
@@ -2,45 +2,77 @@
 
 let rec binSearch target arr =
     match Array.length arr with
-    | 0 -> None
+    | 0 -> false
+    | 1 -> target = Array.head arr
     | i -> 
         let middle = i / 2
         if target < arr.[middle] then
-            if middle = 0 then None 
-            else binSearch target arr.[..middle-1]
+            binSearch target arr.[..middle-1]
         else if target > arr.[middle] then
-            if middle = i then None
-            else binSearch target arr.[middle+1..]
-        else 
-            Some target
+            binSearch target arr.[middle+1..]
+        else true
 
-let findPivotInd arr = 
-    let rec findPivotIndHelper arr firstElt = 
-        match Array.length arr with
-        | 0 -> None
-        | 1 -> None
-        | i ->
-            let middle = i / 2
-            if arr.[middle] > arr.[middle + 1] then
-                // This is the pivot
-                Some middle
-            else if arr.[middle] > firstElt then
-                // pivot is in right half
-                if middle = i then None 
-                else findPivotIndHelper arr.[middle+1..] firstElt
-            else
-                // pivot in left half
-                if middle = 0 then None 
-                else findPivotIndHelper arr.[..middle-1] firstElt
-    match Array.length arr with 
-    | 0 -> None
-    | _ ->
-        findPivotIndHelper arr arr.[0]
+// arr must be sorted and rotated 
+// e.g. [|3;4;5;1;2|]
+let rec find target arr = 
+    match Array.length arr with
+    | 0 -> false
+    | 1 -> target = Array.head arr
+    | i -> 
+        let middle = i / 2 //  5->2, 1->0
+        if target = arr.[middle] then true
+        else 
+            if arr.[0] < arr.[middle] then
+            // left half of array is sorted
+                if target >= arr.[0] && target < arr.[middle] then
+                    // target is bounded between first and middle - 1
+                    find target arr.[..middle-1]
+                else 
+                    // target is in the unsorted half
+                    find target arr.[middle+1..]
+            else 
+                // right half of array is sorted
+                if target > arr.[middle] && target <= (Array.last arr) then
+                    // target is in the sorted half
+                    find target arr.[middle+1..]
+                else 
+                    // target is in the unsorted half
+                    find target arr.[..middle-1]
+
+let rec findAlt target arr = 
+    let rec find2 target arr l r =
+        match r - l with
+        | 0 -> false
+        | 1 -> target = Array.get arr l
+        | i -> 
+            let middle = i / 2 //  5->2, 1->0
+            let leftElt = Array.get arr l
+            let rightElt = Array.get arr r
+            let midElt = Array.get arr middle
+            if target = midElt then true
+            else 
+                if leftElt < midElt then
+                // left half of array is sorted
+                    if target >= leftElt && target < midElt then
+                        // target is bounded between first and middle - 1
+                        find2 target arr l (middle-1)
+                    else 
+                        // target is in the unsorted half
+                        find2 target arr (middle+1) r
+                else 
+                    // right half of array is sorted
+                    if target > midElt && target <= rightElt then
+                        // target is in the sorted half
+                        find2 target arr (middle+1) r
+                    else 
+                        // target is in the unsorted half
+                        find2 target arr l (middle-1)
+    find2 target arr 0 (Array.length arr - 1)
+
+
 
 
 
 [<EntryPoint>]
 let main argv =
-    findPivotInd [|1;2|]
-    |> printfn "%A"
     0 // return an integer exit code
