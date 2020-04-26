31a56276d726f83a248f3729ae022cde2d5a9d1e
ttay <ttay@umich.edu>
2020-04-26 23:59:04 +0800

    added two new dirs

---
+++ tracing_gc/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+let rec loop tmp i =
+    if i <= 0 then tmp else
+    let tmp2 = loop (Array.copy tmp) (i-1)
+    tmp2.[0] <- tmp2.[0] + 1
+    tmp2
+
+
+[<EntryPoint>]
+let main _ =
+    let a = loop [|1..10000|] 10000
+    printfn "%d" a.[0]
+    0
